d390fc73fca774ffc882d51d3c196bfa8ef7f6d4
liuzheng712 <liuzheng712@gmail.com>
2015-07-28 18:50:48 +0800

    update

--- static/js/ace/snippets/latex.js
+++ static/js/ace/snippets/latex.js
@@ -1,1 +1,21 @@
-define("ace/snippets/latex",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="latex"})
+define("ace/snippets/latex",["require","exports","module"], function(require, exports, module) {
+"use strict";
+
+exports.snippetText ="# documentclass\n\
+snippet documentclass\n\
+	documentclass[${1?:12pt,a4paper}]\{${2:book}\} \n\
+	${3?:body}\n\
+# begin\n\
+snippet begin\n\
+	begin\{${1?:function_name}\}[${2:argument}] \n\
+		${3:}\n\
+	\\end\{${1}\}\n\";\
+# begin{\n\
+snippet begin{\n\
+	begin\{${1?:function_name}\}[${2:argument}] \n\
+		${3:}\n\
+	\\end\{${1}\}\n\";\
+	";
+exports.scope = "latex";
+
+});
